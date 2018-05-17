5a95eaf76e9066daedbda4074241feffdef35f3c
malli <malli@malli-PC>
2018-05-17 16:40:55 +0530

    1st comment

---
+++ src/Employee.java
@@ -0,0 +1,32 @@
+
+
+public class Employee {
+	
+	private int eid;
+	private String ename;
+	private int esal;
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public int getEsal() {
+		return esal;
+	}
+	public void setEsal(int esal) {
+		this.esal = esal;
+	}
+	@Override
+	public String toString() {
+		return "Employee [eid=" + eid + ", ename=" + ename + ", esal=" + esal + "]";
+	}
+	
+
+}
