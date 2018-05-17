7f4a26b23305d18e8167f5af236ba0fbfeb36cd5
malli <malli@malli-PC>
2018-05-17 20:04:34 +0530

    2nd change

---
+++ src/dfe.java
@@ -0,0 +1,21 @@
+
+/**
+ * @author malli
+ *
+ */
+/**
+ * @author malli
+ *
+ */
+public class dfe {
+public int getDf() {
+		return df;
+	}
+
+	public void setDf(int df) {
+		this.df = df;
+	}
+
+private int df;
+
+}
